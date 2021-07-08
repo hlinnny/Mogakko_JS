81cc53f87ea5732e5ed74f9c342e1297fbc4dc9f
hlinnny <77440476+hlinnny@users.noreply.github.com>
2021-07-08 12:02:37 +0900

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,16 @@
+/* index.js */
+
+function mo_btn() {
+    var span = document.getElementsByTagName('span')
+    span[0].innerHTML = "모처럼이니"
+}
+
+function gak_btn() {
+    var span = document.getElementsByTagName('span')
+    span[1].innerHTML = "각잡고 열심히 하면"
+}
+
+function ko_btn() {
+    var span = document.getElementsByTagName('span')
+    span[2].innerHTML = "코린이 탈출이다 !"
+}
